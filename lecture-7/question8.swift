e495c3eaa5c5d839f44c3477e56747102e78ec78
Emdowoinn <88248796+Emdowoinn@users.noreply.github.com>
2021-12-07 00:02:57 +0900

    add lecture 7,8

---
+++ lecture-7/question8.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+var syouhinTanka:Int 
+syouhinTanka = Int(readLine()!)! 
+
+var kounyuuSuuryou:Int 
+var shiharaiKingaku:Int
+
+while syouhinTanka > 0 {
+    kounyuuSuuryou = Int(readLine()!)!
+
+    switch kounyuuSuuryou{
+        case 30...:
+            syouhinTanka = syouhinTanka*7/10
+        case 20..<30:
+            syouhinTanka = syouhinTanka*8/10
+        case 10..<20:
+            syouhinTanka = syouhinTanka*9/10   
+        default:
+            break
+    }
+
+    shiharaiKingaku = syouhinTanka * kounyuuSuuryou
+    print("商品単価は\(syouhinTanka)円です。")
+    print("購入数量は\(kounyuuSuuryou)個です。") 
+    print("支払金額は\(shiharaiKingaku)円です。")
+
+    syouhinTanka = Int(readLine()!)!
+}
